42ef3687b47a19c439f5671e145e43804fa1cef8
MrBrown6210 <bite6210@gmail.com>
2021-03-19 16:34:55 +0700

    Clear useless routes & models

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,8 @@
-import { Router } from 'express';
-import { TagRoutes } from './tag-routes';
-import { UsersRoutes } from './users-routes';
-import { ProfilesRoutes } from './profiles-routes';
-import { ArticlesRoutes } from './articles-routes';
-
+import { Router } from "express";
+import { UsersRoutes } from "./users-routes";
 
 const router: Router = Router();
 
-
-router.use('/tags', TagRoutes);
-router.use('/', UsersRoutes);
-router.use('/profiles', ProfilesRoutes);
-router.use('/articles', ArticlesRoutes);
-
+router.use("/", UsersRoutes);
 
 export const MainRouter: Router = router;
