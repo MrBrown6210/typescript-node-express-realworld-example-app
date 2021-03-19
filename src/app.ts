290fdf55d0d281db277705f93be2befd17978ea2
MrBrown6210 <bite6210@gmail.com>
2021-03-19 16:36:48 +0700

    Add cors package

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,34 @@
-import express from 'express';
-import { Application } from 'express';
-import * as bodyParser from 'body-parser';
-import { MainRouter } from './routes';
-import { loadErrorHandlers } from './utilities/error-handling';
-import session from 'express-session';
+import express from "express";
+import { Application } from "express";
+import * as bodyParser from "body-parser";
+import { MainRouter } from "./routes";
+import { loadErrorHandlers } from "./utilities/error-handling";
+import session from "express-session";
+import cors from "cors";
 import helmet from "helmet";
 import compression from "compression";
 import { SESSION_SECRET } from "./utilities/secrets";
-import './database'; // initialize database
-import './utilities/passport'
-
-
+import "./database"; // initialize database
+import "./utilities/passport";
 
 const app: Application = express();
 
+app.use(cors());
 app.use(helmet());
 app.use(compression());
 app.use(bodyParser.json());
-app.use(session({
+app.use(
+  session({
     secret: SESSION_SECRET,
     cookie: {
-      maxAge: 60000
+      maxAge: 60000,
     },
-    resave           : false,
-    saveUninitialized: false
-  }
-));
-app.use('/api', MainRouter);
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use("/api", MainRouter);
 
 loadErrorHandlers(app);
-
 
 export default app;
