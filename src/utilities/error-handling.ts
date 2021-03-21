fdcb5171a58ae33a5e2a5c1bd8d0cb5d9ce389fd
MrBrown6210 <bite6210@gmail.com>
2021-03-21 21:05:35 +0700

    Change Debug style when route error

--- src/utilities/error-handling.ts
+++ src/utilities/error-handling.ts
@@ -1,41 +1,47 @@
-import { Application, Request, Response } from 'express';
+import { Application, Request, Response } from "express";
 import { IS_PRODUCTION } from "./secrets";
 import logger from "./logger";
 
 export function loadErrorHandlers(app: Application) {
-
   // catch 404 errors and forward to error handler
   app.use((req, res, next) => {
-
     interface BetterError extends Error {
       status?: number;
     }
 
-    const err: BetterError = new Error('Not Found');
-    err.status             = 404;
+    const err: BetterError = new Error("Not Found");
+    err.status = 404;
     next(err);
   });
 
   app.use((err: any, req: Request, res: Response, next: any) => {
-
-    if (err.name === 'ValidationError') {
+    if (err.name === "ValidationError") {
       return res.status(422).json({
-        errors: Object.keys(err.errors).reduce(function (errors: any, key: string) {
+        errors: Object.keys(err.errors).reduce(function (
+          errors: any,
+          key: string
+        ) {
           errors[key] = err.errors[key].message;
 
           return errors;
-        }, {})
+        },
+        {}),
       });
     }
 
-    logger.error(err);
+    err.url = req.url;
+
+    logger.error({
+      url: req.url,
+      ...err,
+    });
+    // logger.error(err);
     res.status(err.status || 500);
     res.json({
       errors: {
         message: err.message,
-        error  : !IS_PRODUCTION ? err : {}
-      }
+        error: !IS_PRODUCTION ? err : {},
+      },
     });
   });
-
 }
